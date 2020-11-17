b1c9480a09ca1bd69e6a5ae8d93db1c90d23f811
duooi <duooiChen@163.com>
2020-11-17 13:30:03 +0800

    20201117 1329

---
+++ vueex/src/main.js
@@ -0,0 +1,14 @@
+import {add,mul} from './utils'
+
+console.log(
+    add(1,2),
+    mul(3,4)
+)
+
+require('./css/normalize.css')
+require('./less/normalize.less')
+
+document.write('Hello webpack')
+
+const x = 1;
+const y = 2;
---
+++ vueex/webpack.config.js
@@ -0,0 +1,41 @@
+const {resolve} = require('path')
+
+module.exports = {
+    entry:'./src/main.js',
+    output:{
+        path:resolve(__dirname,'dist'),
+        filename:'bundle.js',
+        publicPath:'dist/'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.(jpg|png|gif|jpeg)$/i,
+                use:{
+                    loader:'file-loader',
+                    options:{
+                        name:'img/[name].[hash:8].[ext]'
+                    }
+                }
+            },
+            {
+                test:/\.less$/,
+                use:['style-loader','css-loader','less-loader']
+            },
+            {
+                test:/\.js$/,
+                exclude:/(node_modules|bower_components)/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
